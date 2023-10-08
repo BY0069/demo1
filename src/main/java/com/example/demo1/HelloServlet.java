a219481a41e626fe3889508e634e3f0ea5f145cb
Grime <grimebnh@gmail.com>
2023-10-08 22:24:19 +0800

    demo test

--- src/main/java/com/example/demo1/HelloServlet.java
+++ src/main/java/com/example/demo1/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello demo!";
+        message = "hello demo!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
